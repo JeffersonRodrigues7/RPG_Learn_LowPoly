0a4cf82d4a6b1fc5e716bd3860a32a5aff1f1453
Jefferson Leite Rodrigues Dantas <jefferson_rodrigues7@hotmail.com>
2023-11-18 09:24:35 -0300

    fazendo aliado seguir player
    fazendo aliado seguir player

---
+++ RPG_Learn_LowPoly/Assets/Scripts/Ally/AllyMovement.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+using RPG.Player.Attack;
+using System;
+
+
+
+namespace RPG.Ally.Movement
+{
+    public class AllyMovement : MonoBehaviour
+    {
+        [Header("Basic")]
+        [SerializeField] private Transform leader;
+        [SerializeField] private float minDistanceToWalk  = 5.0f; //Distância pro leader
+        [SerializeField] private float minDistanceToRun = 10.0f; //Distância pro leader
+        [Space]
+
+        [Header("Movimentacao")]
+        [SerializeField] private float walkSpeed = 5.0f; //Velocidade do jogador ao andar
+        [SerializeField] private float runSpeed = 10.0f; //Velocidade do jogador ao correr
+        [Space]
+
+        [Header("Debug")]
+        [SerializeField] private bool isWalking = false;
+        [SerializeField] private bool isRunning = false;
+
+        private Animator animator; //Componente animator
+        private Rigidbody rb; //Componente rigidibody
+        private NavMeshAgent navMeshAgent; //Componente navMeshAgent
+
+        private float currentDistanceToLeader;
+
+        private int walkingHash;
+        private int runningHash;
+
+        private float currentSpeed;
+
+        private void Awake()
+        {
+            // Inicializa os componentes e vari?veis necess?rias quando o objeto ? criado
+            animator = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            walkingHash = Animator.StringToHash("isWalking");
+            runningHash = Animator.StringToHash("isRunning");
+        }
+
+        // Update is called once per frame
+        void Update()
+        { 
+            currentDistanceToLeader = Vector3.Distance(transform.position, leader.position);
+
+            if(currentDistanceToLeader >= minDistanceToWalk)
+                navMeshAgent.SetDestination(leader.position);
+
+            if (currentDistanceToLeader >= minDistanceToRun) //Correndo
+            {
+                updateMoveAnimation(true, true);
+            }
+            else if (currentDistanceToLeader >= minDistanceToWalk) //Andando
+            {
+                updateMoveAnimation(true, false);
+            }
+            else if(navMeshAgent.hasPath) //Parado
+            {
+                navMeshAgent.ResetPath();
+                updateMoveAnimation(false, false);
+            }
+        }
+
+        //Atualiza??o anima??o de mvimento
+        private void updateMoveAnimation(bool _isWalking, bool _isRunning)
+        {
+            isWalking = _isWalking;
+            isRunning = _isRunning;
+
+            currentSpeed = isRunning ? runSpeed : walkSpeed;
+            navMeshAgent.speed = currentSpeed;
+
+            animator.SetBool(walkingHash, isWalking);//Para que ele chegue na anima??o de correr, ? necess?rio que esteja andando
+            animator.SetBool(runningHash, isRunning);
+        }
+
+    }
+
+}
